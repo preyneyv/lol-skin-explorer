4c7c33a378ca47104838e4fb4d19b086a5694836
Pranav Nutalapati <pranavnutalapati@gmail.com>
2021-12-02 02:01:13 -0500

    Fixed footer typo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,7 +24,7 @@ export function Footer() {
           >
             @preyneyv
           </a>
-          .
+          .{" "}
           <a
             href="https://github.com/preyneyv/lol-skin-explorer"
             target="_blank"
