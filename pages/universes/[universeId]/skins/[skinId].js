3d0669b3ae1e566b810721dcae845e110da2e23c
Pranav Nutalapati <pranavnutalapati@gmail.com>
2021-12-20 23:41:41 -0500

    Universes viewer bugfix

--- pages/universes/[universeId]/skins/[skinId].js
+++ pages/universes/[universeId]/skins/[skinId].js
@@ -87,7 +87,7 @@ export async function getStaticPaths() {
             if (!u) return null;
             return {
               params: {
-                universeId: u.id,
+                universeId: u.id.toString(),
                 skinId: skin.id.toString(),
               },
             };
