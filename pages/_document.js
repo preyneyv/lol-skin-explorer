1cede07e985936fd59024f67d60d5e57af4dca1f
Pranav Nutalapati <pranavnutalapati@gmail.com>
2025-04-28 11:40:47 -0400

    add grow script

--- pages/_document.js
+++ pages/_document.js
@@ -73,6 +73,11 @@ class Document extends BaseDocument {
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3297862613403903"
                 crossOrigin="anonymous"
               ></script>
+              <script data-grow-initializer="" dangerouslySetInnerHTML={{
+                __html: `
+                !(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTplZjc4NGU1NC02OTExLTQ4MGItOTk3NS1jNzFiY2Y3NDY1YTA=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
+                `,
+              }} />
             </>
           )}
         </Head>
