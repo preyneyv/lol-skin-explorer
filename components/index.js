3e5df3a60863eae443915e0b77497b5be3d5ae6f
Pranav Nutalapati <pranavnutalapati@gmail.com>
2025-06-02 10:55:00 -0400

    feat: enhance ad layout and implement dynamic height adjustments for sticky ads

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import { useEffect, useRef } from "react";
 import { Footer, FooterContainer } from "./footer";
 import { Header } from "./header";
 import styles from "./styles.module.scss";
@@ -23,26 +24,68 @@ export function Layout({ children, flat, backTo, withNew }) {
 
 export function LayoutWithAds({ children, flat, backTo, withNew }) {
   // useRouteInterceptor();
+
+  const footerRef = useRef(null);
+  useEffect(() => {
+    if (!footerRef.current) {
+      return;
+    }
+
+    const mutationObserver = new MutationObserver((cb) => {
+      const ads = document.querySelectorAll(
+        "body > span > span > span > iframe"
+      );
+      let hasMobile = false,
+        hasDesktop = false;
+      ads.forEach((ad) => {
+        if (ad.clientHeight === 50) {
+          hasMobile = true;
+        } else {
+          hasDesktop = true;
+        }
+      });
+
+      footerRef.current.style.setProperty(
+        "--mobile-h",
+        hasMobile ? "50px" : "0px"
+      );
+      footerRef.current.style.setProperty(
+        "--desktop-h",
+        hasDesktop ? "90px" : "0px"
+      );
+
+      console.log(hasMobile, hasDesktop);
+    });
+    mutationObserver.observe(document.body, {
+      subtree: true,
+      childList: true,
+    });
+    return () => {
+      mutationObserver.disconnect();
+    };
+  }, [footerRef]);
+
   return (
     <FooterContainer>
       <div>
         <Header {...{ flat, backTo }} />
-        <div className={styles.adBanner}>
-          <VenatusAd placementName="billboard" />
-          <VenatusAd placementName="mobile_banner" />
-        </div>
         <div className={styles.adLayout}>
-          <div></div>
-          <div>
+          <div className={styles.sidebar}>
+            <div className={styles.sidebarAdContainer}>
+              <VenatusAd placementName="double_mpu" />
+            </div>
+          </div>
+          <div className={styles.main}>
+            <div className={styles.adBanner}>
+              <VenatusAd placementName="billboard" />
+              <VenatusAd placementName="mobile_banner" />
+            </div>
             {withNew && <LazyNewAdditions />}
             {children}
           </div>
           <div></div>
         </div>
       </div>
-      <VenatusAd placementName="video" />
-      <VenatusAd placementName="horizontal_sticky" />
-      <VenatusAd placementName="mobile_horizontal_sticky" />
 
       <Footer {...{ flat }} />
 
@@ -52,6 +95,10 @@ export function LayoutWithAds({ children, flat, backTo, withNew }) {
         <a href="#" onClick={() => (window.location.href = "/")}>
           Exit
         </a>
+      </div>
+      <div ref={footerRef} className={styles.stickyAdContainer}>
+        <VenatusAd placementName="mobile_horizontal_sticky" />
+        <VenatusAd placementName="horizontal_sticky" />
       </div>
     </FooterContainer>
   );
